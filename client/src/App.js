e81d2a89f26682ddf6fac262d002f988520c6a8f
Thomalita Burbank <thomalita37@gmail.com>
2022-12-09 14:09:48 -0700

    app.js setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,59 @@
+import { Route, Routes } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { CheckSession, SignInUser } from './services/auth'
+import Register from './Pages/Register'
+import Login from './Pages/Login'
 import './index.css';
-import Test from './components/TestComponent';
 
 function App() {
+  const [authenticated, toggleAuthenticated] = useState(false)
+  const [user, setUser] = useState(null)
+
+  const handleLogOut = () => {
+    setUser(null)
+    toggleAuthenticated(false)
+    localStorage.clear()
+  }
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+    toggleAuthenticated(true)
+  }
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      checkToken()
+    }
+  }, [])
+
   return (
-    <div>
-      <Test />
+    <div className="App">
+      <header className="nav">
+        <Nav
+          authenticated={authenticated}
+          user={user}
+          handleLogOut={handleLogOut}
+        />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/:animeId"
+            element={<Anime user={user} authenticated={authenticated} />}
+          />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/login"
+            element={
+              <Login
+                setUser={setUser}
+                toggleAuthenticated={toggleAuthenticated}
+              />
+            }
+          />
+        </Routes>
+      </main>
     </div>
   );
 }
