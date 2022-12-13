73bd50c506d216f41d0e74b4598cfff284c02131
Thomalita Burbank <thomalita37@gmail.com>
2022-12-13 13:02:37 -0700

    added background image in home

--- client/src/Components/AnimeCard.jsx
+++ client/src/Components/AnimeCard.jsx
@@ -16,18 +16,28 @@ export const AnimeCard = ({name, image, details, id, api}) => {
 
   return (
     <div>
+        <ul>
+         <li>  
         <Link to={`/${id}`}>
             <img id='anime-card' src={image}/>
             <h2>{name}</h2>
             <p>{details}</p>
         </Link>
+        </li> 
+        <li>
         <Link id='addAnime' to='/add-anime'>
             <button>Add</button>
         </Link>
+        </li>
+        <li>
         <Link id='updateAnime' to={`/anime/${id}/updateAnime`}>
             <button>Update</button>
         </Link>
-        <button onClick={() => handleDelete(id)}>Delete</button>
+        </li>
+        <li>
+        <button id='deleteAnime' onClick={() => handleDelete(id)}>Delete</button>
+        </li>
+        </ul>
     </div>
   )
 }
