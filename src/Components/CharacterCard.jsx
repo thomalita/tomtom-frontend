3f4ecf2bf81d7c8f178edc344076a6c697076a04
Thomalita Burbank <thomalita37@gmail.com>
2022-12-14 15:13:37 -0700

    added character changes

--- src/Components/CharacterCard.jsx
+++ src/Components/CharacterCard.jsx
@@ -7,9 +7,6 @@ export const CharacterCard = ({ name, image, details}) => {
         <img id='character-card' src={image}/>
         <h2>{name}</h2>
         <p>{details}</p>
-        <Link id='add-character' to='/anime/:id'>
-        <h4>Add Character</h4>
-        </Link>
     </div>
   )
 }
