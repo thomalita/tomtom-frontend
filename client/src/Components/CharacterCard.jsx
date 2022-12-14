fb7d80369ee22f98a44b575a641346e5a23f9527
Thomalita Burbank <thomalita37@gmail.com>
2022-12-14 14:28:07 -0700

    add character

--- client/src/Components/CharacterCard.jsx
+++ client/src/Components/CharacterCard.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 
 export const CharacterCard = ({ name, image, details}) => {
   return (
-    <div>
+    <div id='add-character'>
         <img id='character-card' src={image}/>
         <h2>{name}</h2>
         <p>{details}</p>
-        <Link id='addCharacter' to='/anime/:id'>
+        <Link id='add-character' to='/anime/:id'>
         <h4>Add Character</h4>
         </Link>
     </div>
--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -14,7 +14,9 @@ const Nav = ({ authenticated, user, handleLogOut}) => {
         <Link id='addAnime' to='/add-anime'>
         <h4>Add Anime</h4>
         </Link>
-
+        <Link id='add-character' to='/anime/:id'>
+        <h4>Add Character</h4>
+        </Link>
         <Link id="sign-out" onClick={handleLogOut} to="/">
         <h4>Sign Out</h4>
         </Link>
